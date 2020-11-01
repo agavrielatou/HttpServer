3829f45ef9df8395d7c58498767d15f7b53e16ab
Anna Gavrielatou <agavrielatou@synamedia.com>
2020-11-01 14:55:37 +0000

    Tested successfully with valid and invalid urls

--- main.go
+++ main.go
@@ -19,6 +19,40 @@ const (
   dbname   = "postgres"
 )
 
+func findUrlInDB(url string) bool {
+    // connect to AWS RDS that had sample malformed URLs
+    psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+      "password=%s dbname=%s sslmode=disable",
+      host, port, user, password, dbname)
+
+    db, err := sql.Open("postgres", psqlInfo)
+    if err != nil {
+        panic(fmt.Sprintf("Error while opening connection to DB: %s", err))
+    }
+
+    err = db.Ping()
+    if err != nil {
+        panic(fmt.Sprintf("Error while pinging DB: %s", err))
+    }
+
+    sql := "select * from malformedurl where url like url"
+    data, err := db.Query(sql)
+    if err != nil {
+        panic(fmt.Sprintf("Error query: %s", err))
+    }
+    for data.Next() {
+        var urlRes string
+        err = data.Scan(&urlRes)
+        if err != nil {
+            panic(fmt.Sprintf("Error scan: %s", err))
+        }
+        if url == urlRes {
+            return true
+        }
+    }
+    return false
+}
+
 // need to support GET requests:
 // /urlinfo/1/{hostname_and_port}/{original_path_and_query_string}
 func getHandler(w http.ResponseWriter, r *http.Request) {
@@ -32,41 +66,27 @@ func getHandler(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    url := strings.Split(r.URL.Path, "/urlinfo/1/")
-//    if !isValid(url) {
-//      fmt.Fprintf(w, "Not valid URL")
-//      return
-//    }
+    prefix := "/urlinfo/1/"
+    pos := strings.LastIndex(r.URL.Path, prefix)
+    if pos == -1 {
+        return
+    }
+    adjustedPos := pos + len(prefix)
+    if adjustedPos >= len(r.URL.Path) {
+        return
+    }
+    url := r.URL.Path[adjustedPos:len(r.URL.Path)]
 
-    fmt.Fprintf(w, "Valid URL: %s", url)
+    urlFound := findUrlInDB(url)
+    if urlFound {
+        fmt.Fprintf(w, "Invalid URL: %s", url)
+        return
+    }
+
+    fmt.Fprintf(w, "Valid myURL: %s", url)
 }
 
 func main() {
-    // connect to AWS RDS that had sample malformed URLs
-
-    psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-      "password=%s dbname=%s sslmode=disable",
-      host, port, user, password, dbname)
-
-    db, err := sql.Open("postgres", psqlInfo)
-    if err != nil {
-      fmt.Printf("Error while opening connection to DB: %s", err)
-      panic(err)
-    }
-
-    err = db.Ping()
-    if err != nil {
-      fmt.Printf("Error while pinging DB: %s", err)
-      panic(err)
-    }
-    fmt.Printf("Successfully connected!")
-
-    sql := "select * from malformedurl"
-    data, err := db.Query(sql)
-    if err != nil {
-        fmt.Printf("Error query: %s", err)
-    }
-    fmt.Printf("Successful query: %s", data)
 
     // Set up Http request handler
     http.HandleFunc("/", getHandler)
